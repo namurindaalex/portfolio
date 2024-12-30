2345c060444efb1bfb344c73a4325567f727da77
namurindaalex <namurindaalex43@gmail.com>
2024-12-30 14:20:51 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 60,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
